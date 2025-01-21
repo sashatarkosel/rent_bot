a9dc036de46b9315196cce4856fc65001366d8ec
sashatarkosel <sashatarkosel@gmail.com>
2025-01-21 10:20:55 +0500

    feat: add "Explain definitions" feature

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import openai
 from bot_keys import *
 
@@ -8,18 +9,11 @@ openai.api_key = API_KEY
 # Инициализация бота
 bot = telebot.TeleBot(BOT_TOKEN)
 
+user_context = {}  # Словарь с контекстом
+
 
 # Функция для обращения к OpenAI
-def get_gpt_response(user_message):
-    # Описание контекста для модели
-    system_prompt = (
-        "Вы эксперт, который помогает переписывать тексты законов, делая их простыми, ясными и доступными для широкой "
-        "аудитории, не обладающей юридическими знаниями. Сохраняйте ключевые аспекты и суть закона, избегая сложных "
-        "формулировок и юридического жаргона. Включайте пояснения сложных моментов и примеры (в формате: <i>Пример:</i>). "
-        "Также форматируй текст в формате: <b>Заголовок</b> "
-        "Это пример текста, в котором <b>жирный текст</b>, <i>курсивный текст</i>, <s>зачёркнутый текст</s>, "
-        "<u>подчёркнутый текст</u> и <code>код</code> вместо ``` оформлены в соответствии с указанным форматом."
-        )
+def get_gpt_response(user_message, system_prompt):
     try:
         # Запрос к модели
         response = openai.ChatCompletion.create(
@@ -49,12 +43,63 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def handle_text(message):
     user_message = message.text
-    bot.reply_to(message, "Подождите, думаю...")
-    response = get_gpt_response(user_message)
-    with open('запросы.txt', 'a') as responses:
-        responses.write(f'{message.from_user.username}:\n{user_message}\n\nbot:\n{response}\n-\n-\n')
-    print(response)
-    bot.send_message(message.chat.id, response, parse_mode='HTML')
+    username = message.from_user.username
+
+    if username not in user_context:
+        user_context[username] = []
+
+    user_context[username].append(message.text)
+    context = " ".join(user_context[username][-3:])  # Храним только последние 3 сообщения
+
+    if user_message == "Очистить контекст":
+        # Очищение контекста
+        user_context[message.from_user.username] = []
+        bot.reply_to(message, "Контекст сброшен.")
+    elif user_message == 'Объяснить термины':
+        system_prompt = (
+            'Добавь список сложных терминов из твоего предыдущего'
+            ' ответа с их краткими определениями. Например: '
+            '<i>Термин:</i> Конституция — основной закон государства.'
+        )
+
+        bot.reply_to(message, "Подождите, думаю...")
+        response = get_gpt_response(context, system_prompt)
+
+        # Логирование
+        with open('запросы.txt', 'a') as responses:
+            responses.write(f'{username}:\n{user_message}\n\nbot:\n{response}\n-\n-\n')
+        print(response)
+
+        # Создание клавиатуры
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(KeyboardButton("Очистить контекст"))
+        markup.add(KeyboardButton("Объяснить термины"))
+
+        bot.send_message(message.chat.id, response, parse_mode='HTML', reply_markup=markup)
+    else:
+        # Описание контекста для модели
+        system_prompt = (
+            "Вы эксперт, который помогает переписывать тексты законов, делая их простыми, ясными и доступными для широкой "
+            "аудитории, не обладающей юридическими знаниями. Сохраняйте ключевые аспекты и суть закона, избегая сложных "
+            "формулировок и юридического жаргона. Включайте пояснения сложных моментов и примеры (в формате: <i>Пример:</i>). "
+            "Также форматируй текст в формате: <b>Заголовок</b> "
+            "Это пример текста, в котором <b>жирный текст</b>, <i>курсивный текст</i>, <s>зачёркнутый текст</s>, "
+            "<u>подчёркнутый текст</u> и <code>код</code> вместо ``` и ** оформлены в соответствии с указанным форматом."
+        )
+        bot.reply_to(message, "Подождите, думаю...")
+        response = get_gpt_response(context, system_prompt)
+
+        # Логирование
+        with open('запросы.txt', 'a') as responses:
+            responses.write(f'{username}:\n{user_message}\n\nbot:\n{response}\n-\n-\n')
+        print(response)
+
+        # Создание клавиатуры
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(KeyboardButton("Очистить контекст"))
+        markup.add(KeyboardButton("Объяснить термины"))
+
+        bot.send_message(message.chat.id, response, parse_mode='HTML', reply_markup=markup)
 
 
 # Запуск бота
